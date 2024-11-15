3432e17f90d3859b922585027a70ff4b53eecc9f
nur arain <145634117+ersyani@users.noreply.github.com>
2024-11-15 13:37:37 +0800

    Add files via upload

---
+++ Loop.java
@@ -0,0 +1,15 @@
+package loop;
+
+public class Loop {
+
+    public static void main(String[] args) {
+     
+     
+        System.out.println("Sequence of Number :");//output mengeluarkan kesemua looping number dari 99-11
+        for (int i = 99; i>= 11; i-=11){//formula utk looping number
+            System.out.println(" "+i);
+        }
+        
+    }
+    
+}
