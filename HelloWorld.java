c109548385d0f9cbbc62c5dafc2b66241a9df15a
nur arain <145634117+ersyani@users.noreply.github.com>
2024-11-15 11:44:59 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,87 @@
+package helloworld.pkg1;
+
+//questions 1
+/*public class HelloWorld { //class name adalah hello World = java file name 
+    public static void main(String[] args){ //method@functions
+        // main method tak boleh ada lebih dari satu 
+    }
+        System.out.println("Hello World!!");//display output}
+//End of main 
+} 
+//End od HelloWorld Class/*
+*/
+
+/*public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("Arain");}
+    //End of main
+    }
+// End of Arain /*
+
+
+
+//question 3
+
+/*public class HelloWorld {
+    public static void main (String[] args ){
+        System.out.print("My name is Arain .");
+        System.out.print ("My interest is read a book .");
+        System.out.println ("My Fav place is home");
+    }
+    // End of main/*
+*/
+
+/*public class HelloWorld {
+    public static void main (String [] args){
+        System.out.println(" Dream salary is 500000 \n dream Job is programming \n Fav food is kek ");
+    }
+}*/
+
+/*public class HelloWorld {
+    public static void main (String [] args ){
+        
+        int num1=5 ,num2=3 , num3=5 , num4=12, num5=1;
+        int add = num1 + num4;
+        int sub = num2 - num5; 
+        int divi = num1 / num1;
+        int multi = num4 *num4;
+        
+       System.out.println("===== THIS IS ARTITHMETICS OPERATORS =====\n");
+       System.out.println("          1. 5 + 12 = " +add);
+       System.out.println("          2. 3 - 1 = " +sub);
+       System.out.println("          3. 5 / 5 = " +divi);
+       System.out.println("          4. 12 X 12 = " + multi);
+       System.out.println("==========================================\n\n");
+       
+    }
+            
+}*/
+
+/*public class HelloWorld{
+    public static void main (String [] args )
+    {
+        System.out.println("----------------------------------");
+        System.out.println("|       Welcome to My shop       |");
+        System.out.println("----------------------------------");
+        System.out.println("|       List of beverages        |");
+        System.out.println("|      1. book : RM 3.00         |");
+        System.out.println("|      2. pencil: RM0.50         | ");
+        System.out.println("|      3. pen :RM 4.00           | ");
+        System.out.println("__________________________________\n");
+        System.out.println("Insert code of your item : 2\n");
+        System.out.println("insert quantity : 2\n");
+        System.out.println("pencil : RM 0.50\n ");
+        System.out.println("Total price : RM 1 .00\n");
+        
+    }
+}*/
+
+
+
+
+
+        
+   
+        
+    
+
